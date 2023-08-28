e72df93bca09ec1c4f2c6a4da6520a86cfc7dc2e
withoutLastNameAndFirstName <eldar.979751@gmail.com>
2023-08-28 18:30:12 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 5;
+        a += 10; // 15
+        a -= 3;  // 12
+        a *= 2;  // 24
+        a /= 6;  // 4
+        a <<= 4; // 64
+        a >>= 2; // 16
+        System.out.println(a);  // 16
+        System.out.println();
+    }
+}
